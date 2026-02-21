233143b5bdd63036cdde9e4e972e1b1af37a8edc
tshapela.d@gmail.com <tshapela.d@gmail.com>
2026-02-21 14:09:17 +0200

    1st c# program that prints "Hello, World!" to the console.

---
+++ c# basics.cs
@@ -0,0 +1,9 @@
+using System;   // Import the System namespace
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");  // Print text to the console
+    }
+}
